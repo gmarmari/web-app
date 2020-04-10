4828dbcb54a1df8d6d9e2ee536e400699010246c
gm <georgemarmaris87@gmail.com>
2020-04-10 10:46:44 +0200

    #1 UserDao and userRepository added

---
+++ persistence/src/main/java/com/marmaris/dao/UserDao.java
@@ -0,0 +1,79 @@
+package com.marmaris.dao;
+
+import com.marmaris.enums.Role;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "role")
+public class UserDao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "role_seq")
+    @SequenceGenerator(name = "role_seq", sequenceName = "role_seq", allocationSize = 1)
+    private Long id;
+
+    @Version
+    private Integer version;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private Role role;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (!(o instanceof UserDao)){
+            return false;
+        }
+
+        final UserDao that = (UserDao) o;
+
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
