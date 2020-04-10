5aa475b4d4c2f5d124326b3e328595d7d19824eb
gm <georgemarmaris87@gmail.com>
2020-04-10 10:47:29 +0200

    #1 UserDto added. EqualsTests written

---
+++ common/src/test/java/com/marmaris/dto/EqualsTests.java
@@ -0,0 +1,38 @@
+package com.marmaris.dto;
+
+
+import com.marmaris.utils.ClassUtils;
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.EqualsVerifierApi;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.core.type.filter.RegexPatternTypeFilter;
+
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+class EqualsTests {
+
+    @ParameterizedTest
+    @MethodSource("getDtoClasses")
+    void equalsContract(String className) throws Exception {
+        // WHEN
+        Class<?> clazz = Class.forName(className);
+
+        // THEN
+        EqualsVerifierApi<?> verifier = EqualsVerifier.forClass(clazz);
+
+        // VERIFY
+        verifier.verify();
+    }
+
+    private static Stream<Arguments> getDtoClasses() {
+        RegexPatternTypeFilter filter = new RegexPatternTypeFilter(Pattern.compile(".*Dto$"));
+        return ClassUtils.getClassesByPackage("com.marmaris.dto", filter)
+                .stream()
+                .map(Arguments::of);
+    }
+
+
+}
